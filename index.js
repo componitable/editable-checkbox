3279cfad62b672a69679dbb310fd9ad6133aa26b
ForbesLindesay <forbes@lindesay.co.uk>
2013-01-18 14:22:01 +0000

    Support live operation

--- index.js
+++ index.js
@@ -4,13 +4,24 @@ var editable = require('editable');
 module.exports = makeEditable;
 function makeEditable(elements, options) {
     options = options || {};
+    if (options.live != false && typeof elements === 'string') {
+        document.addEventListener('change', function (eventArgs) {
+            if (matchesSelector(eventArgs.target, elements) || matchesSelector(eventArgs.target, elements + ' *')) {
+                var element = eventArgs.target.tagName.toLowerCase() === 'input' ? eventArgs.target : eventArgs.target.getElementsByTagName('input')[0];
+                emit('update', element, element.checked);
+            }
+        }, false);
+        return;
+    } else if (options.live === true) {
+      throw new Error('Live option only works when elements is a string.');
+    }
     editable.forEach(elements, function (element) {
-        if (element.tagName === 'input' && element.type === 'checkbox') {
+        if (element.tagName.toLowerCase() === 'input' && element.type.toLowerCase() === 'checkbox') {
             edit(element, options);
         } else {
             var els = element.getElementsByTagName('input');
             for (var i = 0; i < els.length; i++) {
-                if (els[i].type === 'checkbox') {
+                if (els[i].type.toLowerCase() === 'checkbox') {
                     edit(els[i], options);
                 }
             }
